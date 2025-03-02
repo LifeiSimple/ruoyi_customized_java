474dc019e97eb4212f8086b974b96e708c05f434
wangjx <wangjiaxiang97@foxmail.com>
2025-03-02 21:23:46 +0800

    feat(wangjx-admin):重构项目配置并添加日志功能
    - 重构日志配置，使用 logback-spring.xml 替代 logback.xml
    - 新增 application-dev.yml 配置文件，用于开发环境
    - 修改 application.yml，移除日志和 Redis配置
    - 删除 application-druid.yml 文件
    - 更新 .gitignore，添加 log 目录
    - 在 README.md 中添加项目日志配置说明
    - 引入 Lombok 依赖
    - 更新 RuoYiApplication 类，添加应用名称属性

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # 若依 - 脚手架 后端代码
 
 基于若依vue版本（v3.8.9）修改
+
+## 项目日志配置
+
+文件：wangjx-admin/src/main/resources/application-dev.yml
+
+```yaml
+# 日志配置
+logging:
+  level:
+    com.turntable: debug
+    org.springframework: warn
+  file:
+    # 日志存放位置 win10
+    path: E:\project\${spring.application.name}-logs
+
+```
--- wangjx-admin/src/main/java/com/wangjx/RuoYiApplication.java
+++ wangjx-admin/src/main/java/com/wangjx/RuoYiApplication.java
@@ -1,30 +1,31 @@
 package com.wangjx;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * 启动程序
- * 
+ *
  * @author ruoyi
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class RuoYiApplication
-{
-    public static void main(String[] args)
-    {
+@Slf4j
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class RuoYiApplication {
+
+    private static String APPLICATION_NAME;
+
+    @Autowired
+    public RuoYiApplication(Environment environment) {
+        APPLICATION_NAME = environment.getProperty("spring.application.name");
+    }
+    public static void main(String[] args) {
         // System.setProperty("spring.devtools.restart.enabled", "false");
-        SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(RuoYiApplication.class, args);
+        log.info("-------- {} 启动成功 --------", APPLICATION_NAME);
     }
 }
